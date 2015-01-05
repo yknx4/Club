0f771f79c8c348f785d8ca40ca2d9b9f9d268cef
Jorge Figueroa <yknx4@live.com.mx>
2015-01-04 20:36:26 -0600

    fragment

---
+++ app/src/main/java/com/yknx/android/club/fragments/ClubsNavigationDrawerFragment.java
@@ -0,0 +1,218 @@
+package com.yknx.android.club.fragments;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+
+
+import com.yknx.android.club.R;
+import com.yknx.android.club.adapters.ClubAdapter;
+import com.yknx.android.club.model.NavigationItem;
+import com.yknx.android.club.util.Preferences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by poliveira on 24/10/2014.
+ */
+public class ClubsNavigationDrawerFragment extends android.support.v4.app.Fragment implements NavigationDrawerCallbacks {
+    private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
+    private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+    private static final String STATE_SELECTED_TERM = Preferences.PREF_SELECTED_TERM;
+    private NavigationDrawerCallbacks mCallbacks;
+    private RecyclerView mDrawerList;
+    private View mFragmentContainerView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private Spinner mSpinnerTerms;
+    private boolean mUserLearnedDrawer;
+    private boolean mFromSavedInstanceState;
+    private int mCurrentSelectedPosition;
+
+    private OnSpinnerClick mOnSpinnerClick;
+    private Long mClub = ClubAdapter.INVALID_CLUB_ID; //TODO: IMPLEMENT CLUB ID LOADING
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        mDrawerList = (RecyclerView) view.findViewById(R.id.drawerList);
+        mSpinnerTerms = (Spinner) view.findViewById(R.id.spinnerTerms);
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mDrawerList.setLayoutManager(layoutManager);
+        mDrawerList.setHasFixedSize(true);
+
+        final List<NavigationItem> navigationItems = getMenu();
+        NavigationDrawerAdapter adapter = new NavigationDrawerAdapter(navigationItems);
+        adapter.setNavigationDrawerCallbacks(this);
+        mDrawerList.setAdapter(adapter);
+        selectItem(mCurrentSelectedPosition);
+        mSpinnerTerms.setSelection(currentTerm());
+        mSpinnerTerms.setOnItemSelectedListener(mOnSpinnerClick);
+        return view;
+    }
+
+    private int currentTerm() {
+        return Preferences.getSelectedTerm(getActivity(),mClub);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserLearnedDrawer = Boolean.valueOf(Preferences.readSharedSettingString(getActivity(), PREF_USER_LEARNED_DRAWER, "false"));
+        if (savedInstanceState != null) {
+            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+            mFromSavedInstanceState = true;
+        }
+        mOnSpinnerClick = new OnSpinnerClick();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallbacks = (NavigationDrawerCallbacks) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException("Activity must implement NavigationDrawerCallbacks.");
+        }
+    }
+
+    public ActionBarDrawerToggle getActionBarDrawerToggle() {
+        return mActionBarDrawerToggle;
+    }
+
+    public void setActionBarDrawerToggle(ActionBarDrawerToggle actionBarDrawerToggle) {
+        mActionBarDrawerToggle = actionBarDrawerToggle;
+    }
+
+    public void setup(int fragmentId, DrawerLayout drawerLayout, Toolbar toolbar, long mClub) {
+        this.mClub = mClub;
+        mFragmentContainerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                if (!isAdded()) return;
+                ActivityCompat.invalidateOptionsMenu(getActivity());
+                //getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if (!isAdded()) return;
+                if (!mUserLearnedDrawer) {
+                    mUserLearnedDrawer = true;
+                    Preferences.saveSharedSettingString(getActivity(), PREF_USER_LEARNED_DRAWER, "true");
+                }
+                ActivityCompat.invalidateOptionsMenu(getActivity());
+                //getActivity().invalidateOptionsMenu();
+            }
+        };
+
+        if (!mUserLearnedDrawer && !mFromSavedInstanceState)
+            mDrawerLayout.openDrawer(mFragmentContainerView);
+
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mActionBarDrawerToggle.syncState();
+            }
+        });
+
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+    }
+
+    public void openDrawer() {
+        mDrawerLayout.openDrawer(mFragmentContainerView);
+    }
+
+    public void closeDrawer() {
+        mDrawerLayout.closeDrawer(mFragmentContainerView);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
+    }
+
+    public List<NavigationItem> getMenu() {
+        List<NavigationItem> items = new ArrayList<NavigationItem>();
+        items.add(new NavigationItem(getResources().getString(R.string.NavItemAttendance), getResources().getDrawable(R.drawable.ic_action_time)));
+        items.add(new NavigationItem(getResources().getString(R.string.NavItemUsers), getResources().getDrawable(R.drawable.ic_action_person)));
+        return items;
+    }
+
+    void selectItem(int position) {
+        mCurrentSelectedPosition = position;
+        if (mDrawerLayout != null) {
+            mDrawerLayout.closeDrawer(mFragmentContainerView);
+        }
+        if (mCallbacks != null) {
+            mCallbacks.onNavigationDrawerItemSelected(position);
+        }
+        ((NavigationDrawerAdapter) mDrawerList.getAdapter()).selectPosition(position);
+    }
+
+    public boolean isDrawerOpen() {
+        return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(mFragmentContainerView);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(STATE_SELECTED_POSITION, mCurrentSelectedPosition);
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        selectItem(position);
+    }
+
+    public DrawerLayout getDrawerLayout() {
+        return mDrawerLayout;
+    }
+
+    public void setDrawerLayout(DrawerLayout drawerLayout) {
+        mDrawerLayout = drawerLayout;
+    }
+
+
+
+    private class OnSpinnerClick implements AdapterView.OnItemSelectedListener{
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Preferences.saveSharedSettingInt(getActivity(), STATE_SELECTED_TERM, position);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+}
