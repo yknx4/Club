9a11e3db8d91cbf1126d928d9984c5dadaf3ebaa
Jorge Figueroa <yknx4@live.com.mx>
2015-01-04 22:32:37 -0600

    Fix layout issue

--- app/src/main/java/com/yknx/android/club/fragments/NavigationDrawerCallbacks.java
+++
@@ -1,8 +0,0 @@
-package com.yknx.android.club.fragments;
-
-/**
- * Created by poliveira on 27/10/2014.
- */
-public interface NavigationDrawerCallbacks {
-    void onNavigationDrawerItemSelected(int position);
-}
